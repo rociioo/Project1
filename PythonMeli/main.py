fc653ee3304b9dac215399221633d19ab0c039ab
angel manuel montalto <meluugrey@gmail.com>
2020-06-16 17:54:12 -0300

    again

---
+++ PythonMeli/main.py
@@ -0,0 +1,9 @@
+VariableEdad = int(input ("Escribe tu edad"))
+VariableEdad == 25
+VariableMes = "Julio" 
+VariableName = "Meli"
+Variablesigno = {"Cancer" : "Julio", "Aries" : "Marzo" , "Tauro" : "Abril" , "Geminis" : "Junio" , "Leo" : "Agosto" , "Virgo" : "Septiembre" , "Libra" : "Octubre" , "Escorpio" : "Noviembre" , "Sagitario" : "Diciembre" , "Capricornio" : "Enero" , "Acuario" : "Febrero" , "Piscis" : "Mayo"}
+SignoMes = {"Cancer" : ["Junio", "Julio"] , "Aries" : ["Marzo", "Abril"] , "Tauro" : ["Abril", "Mayo"] , "Geminis" : ["Mayo", "Junio"] , "Leo" : ["Julio", "Agosto"] , "Virgo" : ["Agosto", "Septiembre"] , "Libra" : ["Septiembre", "Octubre"], "Escorpio" : ["Octubre", "Noviembre"] , "Sagitario" : ["Noviembre", "Diciembre"] , "Capricornio" : ["Diciembre", "Enero"] , "Acuario" : ["Enero", "Febrero"] , "Piscis" : ["Febrero", "Marzo"]}
+print(VariableMes[0])
+
+
